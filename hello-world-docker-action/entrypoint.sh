7a1041cfd0ffecb8b2c6d6d554d733ed4c6f6248
Robin Breathe <robin@isometry.net>
2023-06-26 17:41:25 +0200

    Tweak shebang

--- hello-world-docker-action/entrypoint.sh
+++ hello-world-docker-action/entrypoint.sh
@@ -1,6 +1,8 @@
-#!/usr/local/bin/bash -l
+#!/usr/bin/env bash
 
 echo "Positional arguments: $*"
 echo "INPUT_* Environment variables: ${!INPUT_*}"
+
+echo "Hello, ${1}!"
 
 echo "time=$(date +%s)" >> $GITHUB_OUTPUT
